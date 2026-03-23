9ba76743255725bab20da5e63ca49dc6fd181ead
eldemirberkay0 <eldemirberkay01@gmail.com>
2026-03-23 23:00:51 +0300

    Add archer flip

--- Assets/Scripts/Tower/ArcherTower.cs
+++ Assets/Scripts/Tower/ArcherTower.cs
@@ -4,18 +4,20 @@ using FlexTimer;
 public class ArcherTower : ProjectileTower
 {
     [SerializeField] private Animator archerAnimator;
+    [SerializeField] private SpriteRenderer archerRenderer;
     private Timer animTimer;
 
-    protected void Awake()
+    protected void Start()
     {
-        archerAnimator.speed = 1 / towerData.shootInterval;
         animTimer = new Timer(0.5f / archerAnimator.speed, () => base.Shoot());
+        archerAnimator.speed = 1 / towerData.shootInterval;
     }
 
     protected override void Shoot()
     {
         animTimer.Start();
         archerAnimator.SetTrigger("ShouldShoot");
+        archerRenderer.flipX = targetEnemy.transform.position.x < transform.position.x;
     }
 
     protected void OnDestroy()
